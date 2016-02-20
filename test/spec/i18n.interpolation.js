08613d2d3ea212061b954ab498a2279791a817f6
Raphael F. Jesus <raphaelfjesus@gmail.com>
2016-02-20 00:55:02 +0000

    project structure for Node.js (pending browser)

---
+++ test/spec/i18n.interpolation.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * Libraries.
+ */
+var chai = require('chai');
+var expect = chai.expect;
+var i18n = require('../../index');
+
+describe('Interpolation', function() {
+    
+  it('TODO', function(done) {
+    expect(false).to.true;
+      
+    done();
+  });
+  
+});
